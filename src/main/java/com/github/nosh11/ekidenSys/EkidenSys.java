a68916753c905921e6be67f56dbe2b2a156afb69
nosh11 <101454424+nosh11@users.noreply.github.com>
2024-09-06 10:51:11 +0900

    initial commit

---
+++ src/main/java/com/github/nosh11/ekidenSys/EkidenSys.java
@@ -0,0 +1,16 @@
+package com.github.nosh11.ekidenSys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EkidenSys extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
